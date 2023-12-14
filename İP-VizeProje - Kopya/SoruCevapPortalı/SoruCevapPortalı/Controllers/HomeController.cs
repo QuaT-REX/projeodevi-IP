1b0f3473ac4086bc14e394981f4556d94adc384a
QuaT-REX <20211129014@ogr.akdeniz.edu.tr>
2023-12-14 18:27:08 +0300

    ProjeÖdevi

---
+++ İP-VizeProje - Kopya/SoruCevapPortalı/SoruCevapPortalı/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoruCevapPortalı.Models;
+using System.Diagnostics;
+
+namespace SoruCevapPortalı.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ İP-VizeProje - Kopya/SoruCevapPortalı/SoruCevapPortalı_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoruCevapPortalı_Model.Models;
+
+namespace SoruCevapPortalı_DataAccess.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+
+        public  ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+
+        public DbSet<Kategori> Kategoriler { get; set; }
+
+    }
+}
